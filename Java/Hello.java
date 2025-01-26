9039d758df29a73871b9dce15e0a1f4f1797a293
jagasia <rjagadeeswaran@Yahoo.com>
2025-01-26 12:30:12 +0530

    26Jan 12:30

---
+++ Java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+	public static void main(String []args)
+	{
+		System.out.println("Hello this is UST Wave 4 batch...");
+		System.out.println("I have modified this program so that ");
+		System.out.println("These changes are now reflecting when jenkins run");
+	}
+}
